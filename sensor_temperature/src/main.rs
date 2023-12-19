c68b1e31408cfc66e09196beb7a43259b5083105
Andrew William Watson <aww@awwsmm.com>
2023-12-19 13:07:42 +0100

    rewrite temperature_sensor without actix

--- sensor_temperature/src/main.rs
+++ sensor_temperature/src/main.rs
@@ -1,55 +1,61 @@
-use actix_web::web::Data;
-use actix_web::{get, web, App, HttpServer};
-use libmdns::Responder;
+use std::io::{BufRead, BufReader, Write};
+use std::net::TcpListener;
+
 use uuid::Uuid;
 
 use sensor::Sensor;
 use sensor_temperature::TemperatureSensor;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let sensor_id = Uuid::new_v4().to_string();
-    let sensor_name = "temperature_sensor";
+fn main() {
+    // Create a daemon
+    let mdns = mdns_sd::ServiceDaemon::new().expect("Failed to create daemon");
 
-    let port: u16 = 13579;
+    let ip = local_ip_address::local_ip().unwrap();
+    let host = ip.clone().to_string();
 
-    let host = local_ip_address::local_ip().unwrap();
-    let host = host.clone().to_string();
+    let port = 8787;
 
     let address = format!("{}:{}", host, port);
 
-    let responder = Responder::new().unwrap();
+    let sensor_id = Uuid::new_v4().to_string();
+    let sensor_name = "temperature_sensor";
 
     let endpoint = "/datum";
-    let txt = format!("path={}", endpoint);
 
-    let _service = responder.register(
-        "_sensor._tcp".to_owned(),
-        String::from(sensor_name),
-        port,
-        &[txt.as_str()],
+    println!("\nCreating new sensor {} at {}", sensor_name, address);
+    println!(
+        "Ask this sensor for data with: curl {}{}",
+        address, endpoint
     );
 
-    #[get("/datum")]
-    async fn get_datum(data: web::Data<TemperatureSensor>) -> String {
-        let datum = &data.get_datum(); // <- get app_name
-        datum.to_string() // <- response with app_name
-    }
+    // Create a service info.
+    let service_type = "_sensor._tcp.local.";
 
-    HttpServer::new(move || {
-        let sensor = TemperatureSensor::new(sensor_id.as_str(), sensor_name);
+    let my_service =
+        mdns_sd::ServiceInfo::new(service_type, sensor_name, host.as_str(), ip, port, None)
+            .unwrap();
 
-        let address = format!("{}:{}", host, port);
-        println!("\nCreating new sensor {} at {}", sensor_name, address);
-        println!(
-            "Ask this sensor for data with: curl {}{}",
-            address, endpoint
+    // Register with the daemon, which publishes the service.
+    mdns.register(my_service)
+        .expect("Failed to register our service");
+
+    let listener = TcpListener::bind(address).unwrap();
+
+    let sensor = TemperatureSensor::new(sensor_id.as_str(), sensor_name);
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        let mut request = String::new();
+
+        BufReader::new(&mut stream).read_line(&mut request).unwrap();
+        println!("received request: {}", request.trim());
+
+        let contents = sensor.get_datum().to_string();
+        let ack = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",
+            contents.len(),
+            contents
         );
-
-        App::new().app_data(Data::new(sensor)).service(get_datum)
-    })
-    .bind(address)?
-    .workers(1) // sensors should be single-threaded
-    .run()
-    .await
+        stream.write_all(ack.as_bytes()).unwrap();
+    }
 }
