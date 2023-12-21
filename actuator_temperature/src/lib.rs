1f8ad32a8bc559a08ff43a696401218f69d05753
davidleacock <david.leacock@innovapost.com>
2023-12-20 17:24:03 -0800

    Implement new command for Temperature Actuator

--- actuator_temperature/src/lib.rs
+++ actuator_temperature/src/lib.rs
@@ -1,10 +1,23 @@
-use actuator::Actuator;
+use actuator::{Actuator, Command};
 use device::{Device, Id, Name};
+use serde::{Serialize, Deserialize};
 
 pub struct TemperatureActuator {
     id: Id,
     name: Name,
 }
+
+#[derive(Serialize, Deserialize)]
+pub enum TemperatureActuatorCommand {
+    SetMaxTemperature(f32)
+}
+
+impl Command for TemperatureActuatorCommand {
+    fn to_string(&self) -> String {
+        serde_json::to_string(self).unwrap()
+    }
+}
+
 
 impl Device for TemperatureActuator {
     fn get_name(&self) -> &Name {
@@ -18,8 +31,21 @@ impl Device for TemperatureActuator {
 
 #[allow(unused_variables)] // remove ASAP
 impl Actuator for TemperatureActuator {
-    fn act(sensor: Id, command: String) {
-        todo!()
+    fn act(&self, sensor: Id, command: String) {
+
+        match serde_json::from_str::<TemperatureActuatorCommand>(&command) {
+            Ok(command_enum) => {
+                match command_enum {
+                    TemperatureActuatorCommand::SetMaxTemperature(temp) => {
+                        println!("Handling SetMaxTemperature: {}", temp);
+                    }
+                }
+
+            }
+            Err(e) => {
+                println!("Error serializing command to TemperatureActuatorCommand");
+            }
+        }
     }
 }
 
